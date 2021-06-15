cfde2fe6734d01b8d114e7b2b35ff5b2d50d8fa5
Leu Family <romanianfobjr@gmail.com>
2021-06-15 12:50:10 -0500

    added arrow function to task 1

--- index.js
+++ index.js
@@ -4,9 +4,17 @@ import { fifaData } from './fifa.js';
 
 /* 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀 Task 1: 🚀🚀🚀🚀🚀🚀🚀🚀🚀🚀
 Practice accessing data by console.log-ing the following pieces of data note, you may want to filter the data first 😉*/
+
+// Regular Function
+// const finals2014 = fifaData.filter(function(item){
+//     return item.Year === 2014 && item.Stage === 'Final';
+// });
+
+// Arrow Function
 const finals2014 = fifaData.filter(function(item){
     return item.Year === 2014 && item.Stage === 'Final';
 });
+
 console.log('Task 1', finals2014);
 //(a) Home Team name for 2014 world cup final
 console.log('Task 1a', finals2014[0]['Home Team Name']);
